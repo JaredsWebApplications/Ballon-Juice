aa8452407bc7dbc96a296894a7bbd7a5851f1d1f
JaredDyreson <jared.dyreson@gmail.com>
2020-11-18 13:42:05 -0800

    it fuckin works

--- runtime/ThreeDimensional.js
+++ runtime/ThreeDimensional.js
@@ -1,57 +1,106 @@
-let _text;
-let Beefed;
-let G;
-let container = [];
-let leftBuffer;
 let flag = true;
 
-var simplePlane = function(simple) {
-    simple.setup = function() {
-        simple.createCanvas(500, 500);
-        simple.background('white');
-        simple.textSize(32);
-        simple.text('word', 10, 30);
-        for(let i = 0; i < container.length; ++i){
-            container[i].drawToScreen();
-        }
-    }
-    
-    simple.draw = function() {
+var nodeFieldPlane = function(node) {
+    node.delegator;
+
+    node.setup = function() {
+        node.createCanvas(500, 500);
+        node.background('white');
+        node.delegator = new PlaneDelegator(node); //<-- pass the p as parameter in the constructor
     }
 }
 
-var anotherPlane = function(s){
-    s.flag = true;
-    s.setup = function() {
-        //s.background('gray');
-        s.createCanvas(s.windowWidth, s.windowHeight, s.WEBGL);
-        s.Beefed = new BalloonBeefed(100, s);
-        s.container = [
+var informationPlane = function(information){
+    information.delegator;
+    information.number = 0;
+    information.Beefed = new BalloonBeefed(100, information);
+    information.container = information.Beefed.information;
 
-            new Cell(new DPV(0, 0, 0), "0"),
-            new Cell(new DPV(30, 100, 0), "2"),
-            new Cell(new DPV(0, 100, 0), "1"),
-            new Cell(new DPV(-30, 100, 0), "1"),
-            new Cell(new DPV(100, 0, 0), "3")
-        ];
+    [information.x, information.y, information.z] = new DPV(0, 0, 0).position();
+
+    information._text;
+    information. _text = information.createGraphics(500, 500);
+    information._text.fill('black');
+    information._text.textAlign(information.CENTER);
+    information._text.textSize(20);
+    information._text.text(`Outdegree: ${information.number}`, 250, 25);
+    information._text.text(`Coordinate: (${information.x}, ${information.y}, ${information.z})`, 250, 50);
+
+
+    //information.container = [
+        //new Cell(new DPV(0, 0, 0), "0"),
+        //new Cell(new DPV(30, 100, 0), "2"),
+        //new Cell(new DPV(0, 100, 0), "1"),
+        //new Cell(new DPV(-30, 100, 0), "1"),
+        //new Cell(new DPV(100, 0, 0), "3")
+    //];
+
+    information.display = function() {
+        information.Beefed.displayInformation();
+        information.Beefed.displayTorus();
     }
-    s.draw = function() {
-       s.Beefed.displayTorus();
-       s.Beefed.displayInformation();
+
+    information.setup = function() {
+        information.createCanvas(information.windowWidth, information.windowHeight, information.WEBGL);
+        information.delegator = new PlaneDelegator(information);
     }
-    s.mousePressed = function() {
-        if(s.mouseX >= -50 && s.mouseX <= 50){
-            alert("in the box");
-            s.flag = true ? (!s.flag) : false;
+
+
+    information.draw = function() {
+
+        if(flag){
+            noLoop();
+        } else {
+
+            information.background(250);
+
+            information.normalMaterial();
+
+            information.translate(240, 0, 0);
+            information.push();
+            information.rotateZ(information.frameCount * 0.01);
+            information.rotateX(information.frameCount * 0.01);
+            information.rotateY(information.frameCount * 0.01);
+            information.torus(70, 20);
+            information.pop();
+
+            information.texture(information._text);
+            information.plane(500, 500);
         }
     }
 }
 
+class PlaneDelegator {
+    constructor(plane) {
+        this.plane = plane; // <-- save the for reuse
+    }
+
+    render() {
+        this.plane.display();
+    }
+}
+
+let plane;
+
+function mouseClicked() {
+    //plane.Beefed.displayInformation();
+    flag = (!flag) ? true : false;
+}
 function setup(){
-    createCanvas(500, 500);
+    createCanvas(50, 50, WEBGL);
+    plane = new p5(informationPlane);
 }
 
 function draw() {
-    var plane = new p5(anotherPlane);
+    //plane.delegator.render();
+    //this.namespace.background(250);
+    //this.namespace.translate(0, 0, 0);
+    //this.namespace.normalMaterial();
+    //this.namespace.push();
+    //this.namespace.rotateZ(this.namespace.frameCount * 0.01);
+    //this.namespace.rotateX(this.namespace.frameCount * 0.01);
+    //this.namespace.rotateY(this.namespace.frameCount * 0.01);
+    //this.namespace.torus(70, 20);
+    //this.namespace.pop();
 }
 
